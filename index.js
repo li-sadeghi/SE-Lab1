a1f51376138a104900bd711505ed29406e8f4a7e
Bozorgmehr Zia <bozorgmehr1381@gmail.com>
2024-07-11 19:01:37 +0330

    feat: add pet promise

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const petPromise = await fetch('https://learnwebcode.github.io/pet-adoption-data/pets.json');
+const pets = await petPromise.json();
+
 function decideAgeText(age){
     if (!age) {
         return "Less than a year old";
