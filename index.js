140dfd0fe1433b428b7e2889a13a7ee742998753
Bozorgmehr Zia <bozorgmehr1381@gmail.com>
2024-07-11 19:06:17 +0330

    feat: add images for pets

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const petPromise = await fetch('https://learnwebcode.github.io/pet-adoption-data/pets.json');
 const pets = await petPromise.json();
 
+const url = 'https://learnwebcode.github.io/pet-adoption-data/pets/'
+
+const template = document.querySelector('#animal-card');
+const wrapper = document.createElement('dev');
+
 function decideAgeText(age){
     if (!age) {
         return "Less than a year old";
@@ -12,3 +17,13 @@ function decideAgeText(age){
         return `${age} Year Old`
     }
 }
+
+pets.forEach(pet => {
+    const clone = template.content.cloneNode(true);
+    clone.querySelector('h3').textContent = pet.name;
+    const img = clone.querySelector('img');
+    img.src = pet.photo;
+    img.alt = `A ${pet.species} named ${pet.name}`;
+    const age = new Date().getFullYear() - pet.birthYear;
+    const ageText = decideAgeText(age); 
+});
